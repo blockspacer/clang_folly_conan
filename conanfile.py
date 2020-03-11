e7b0f8ae9e9ea175a765a41d007bb547f6ff089c
derofim <derofim@example.com>
2020-03-11 18:54:09 +0300

    initial

--- conanfile.py
+++ conanfile.py
@@ -3,36 +3,49 @@ from conans import ConanFile, CMake, tools
 from conans.tools import Version
 from conans.errors import ConanInvalidConfiguration
 
-
 class FollyConan(ConanFile):
-    name = "folly"
-    version = "2019.09.02.00"
+    name = "clang_folly_conan"
+    version = "v2019.01.14.00"
     description = "An open-source C++ components library developed and used at Facebook"
     topics = ("conan", "folly", "facebook", "components", "core", "efficiency")
-    url = "https://github.com/bincrafters/conan-folly"
-    homepage = "https://github.com/facebook/folly"
-    license = "Apache-2.0"
+    #url = "https://github.com/bincrafters/conan-folly" # TODO
+    #homepage = "https://github.com/facebook/folly" # TODO
+    #license = "Apache-2.0" # TODO
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "*.patch"]
     generators = "cmake"
+    commit_hash = "67f61a811de7af0b87453fcc272aba4106c881e1"
+    repo_url = "https://github.com/facebook/folly.git"
+
+    # conan search double-conversion* -r=conan-center
     requires = (
-        "boost/1.71.0",
-        "double-conversion/3.1.5",
-        "gflags/2.2.2",
-        "glog/0.4.0",
-        "libevent/2.1.11",
-        "lz4/1.9.2",
-        "openssl/1.0.2u",
-        "zlib/1.2.11",
-        "zstd/1.4.3",
-        "snappy/1.1.7",
-        "bzip2/1.0.8",
-        "libsodium/1.0.18",
-        "xz_utils/5.2.4",
-        "libdwarf/20191104"
+        "boost/1.71.0@conan/stable",
+        "double-conversion/3.1.1@bincrafters/stable",
+#        "double-conversion/3.1.5@bincrafters/stable",
+        "gflags/2.2.2@bincrafters/stable",
+        "glog/0.4.0@bincrafters/stable",
+        "libevent/2.1.11@bincrafters/stable",
+#        "lz4/1.9.2",
+        "lz4/1.8.3@bincrafters/stable",
+#        "lz4/1.9.2@bincrafters/stable",
+#        "openssl/1.0.2u@conan/stable",
+#        "openssl/1.0.2u",
+        "openssl/1.1.1c",
+        "zlib/1.2.11@conan/stable",
+        "lzma/5.2.4@bincrafters/stable",
+#        "zstd/1.4.3@conan/stable",
+#        "zstd/1.4.3",
+        "zstd/1.3.8@bincrafters/stable",
+        "snappy/1.1.7@bincrafters/stable",
+        "bzip2/1.0.8@conan/stable",
+        "libsodium/1.0.18@bincrafters/stable",
+        "libelf/0.8.13@bincrafters/stable",
+#        "xz_utils/5.2.4@conan-center/stable",
+#        "libdwarf/20191104"
+        "libdwarf/20190505@bincrafters/stable"
     )
 
     @property
@@ -48,6 +61,13 @@ class FollyConan(ConanFile):
 
     def configure(self):
         compiler_version = Version(self.settings.compiler.version.value)
+
+        # NOTE: our patched folly version requires clang
+        if self.settings.compiler != "apple-clang" and \
+             self.settings.compiler != "clang" and \
+             self.settings.compiler != "clang-cl":
+            raise ConanInvalidConfiguration("Use clang")
+
         if self.settings.os == "Windows" and \
             self.settings.compiler == "Visual Studio" and \
             compiler_version < "15":
@@ -73,22 +93,40 @@ class FollyConan(ConanFile):
             raise ConanInvalidConfiguration("Folly could not be built by apple-clang < 8.0")
 
     def requirements(self):
-        if self.settings.os == "Linux" or \
+        print('self.settings.compiler {}'.format(self.settings.compiler))
+
+        # NOTE: our patched folly version requires clang
+        if self.settings.compiler != "apple-clang" and \
+             self.settings.compiler != "clang" and \
+             self.settings.compiler != "clang-cl":
+            raise ConanInvalidConfiguration("Use clang")
+
+        if self.settings.os == "Linux" and \
            self.settings.compiler == "gcc":
             self.requires("libiberty/9.1.0")
 
     def source(self):
-        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
-        extracted_dir = self.name + '-' + self.version
-        os.rename(extracted_dir, self._source_subfolder)
+        #tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
+        #extracted_dir = self.name + '-' + self.version
+        #os.rename(extracted_dir, self._source_subfolder)
+        self.run('git clone -b {} --progress --depth 100 --recursive --recurse-submodules {} {}'.format(self.version, self.repo_url, self._source_subfolder))
+        #self.run('cd {} && ls -artlh && git reset --hard {} && git submodule init && git submodule update'.format(self._source_subfolder, self.commit_hash))
 
     def _configure_cmake(self):
-        cmake = CMake(self)
+        cmake = CMake(self, set_cmake_flags=True)
+        cmake.verbose = True
+
+        cmake.definitions["BUILD_STATIC_LIBS"]="ON"
+        cmake.definitions["BUILD_SHARED_LIBS"]="OFF"
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"]="ON"
+        cmake.definitions["FOLLY_USE_JEMALLOC"]="OFF"
+
         cmake.configure()
         return cmake
 
     def build(self):
-        tools.patch(base_path=self._source_subfolder, patch_file='0001-compiler-options.patch')
+        #tools.patch(base_path=self._source_subfolder, patch_file='0001-compiler-options.patch')
+        tools.patch(base_path=self._source_subfolder, patch_file='0002-clang-cling-conan-support.patch')
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -98,11 +136,16 @@ class FollyConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self) + ["folly"]
+        self.cpp_info.libs = tools.collect_libs(self)
+            # + ["folly"] # https://github.com/conan-io/conan/issues/2193
+
         if self.settings.os == "Linux":
-            self.cpp_info.libs.extend(["pthread", "dl"])
+            self.cpp_info.libs.extend(["pthread", "m", "dl"])
+            if self.settings.compiler == "clang" and self.settings.compiler.libcxx == "libstdc++":
+                self.cpp_info.libs.append("atomic")
         elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             self.cpp_info.libs.extend(["ws2_32", "Iphlpapi", "Crypt32"])
+
         if (self.settings.os == "Linux" and self.settings.compiler == "clang" and
            Version(self.settings.compiler.version.value) == "6" and self.settings.compiler.libcxx == "libstdc++") or \
            (self.settings.os == "Macos" and self.settings.compiler == "apple-clang" and
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,10 +1,184 @@
 #include <cstdlib>
 #include <iostream>
-#include <folly/Format.h>
+#include <iterator>
+#include <exception>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <memory>
+#include <vector>
 
-int main()
+// __has_include is currently supported by GCC and Clang. However GCC 4.9 may have issues and
+// returns 1 for 'defined( __has_include )', while '__has_include' is actually not supported:
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=63662
+#if __has_include(<filesystem>)
+#include <filesystem>
+#else
+#include <experimental/filesystem>
+#endif // __has_include
+
+#include <boost/program_options.hpp>
+#include <boost/optional.hpp>
+#include <boost/optional/optional_io.hpp>
+#include <boost/program_options.hpp>
+#include <boost/utility/in_place_factory.hpp>
+
+
+#include <folly/Format.h>
+#include <folly/ProducerConsumerQueue.h>
+
+//#include <folly/io/async/ScopedEventBaseThread.h>
+#include <folly/executors/CPUThreadPoolExecutor.h>
+#include <folly/executors/GlobalExecutor.h>
+#include <folly/Executor.h>
+#include <folly/Try.h>
+#include <folly/executors/Async.h>
+#include <folly/executors/GlobalExecutor.h>
+#include <folly/futures/Future.h>
+#include <folly/futures/Promise.h>
+#include <folly/String.h>
+#include <folly/FileUtil.h>
+#include <folly/File.h>
+#include <folly/io/IOBufQueue.h>
+#include <folly/Optional.h>
+#include <folly/Memory.h>
+#include <folly/executors/GlobalExecutor.h>
+#include <folly/io/async/EventBaseManager.h>
+#include <folly/init/Init.h>
+#include <folly/logging/Init.h>
+#include <folly/logging/xlog.h>
+#include <folly/logging/RateLimiter.h>
+#include <folly/init/Init.h>
+#include <folly/Singleton.h>
+#include <folly/logging/Init.h>
+#include <folly/portability/Config.h>
+#include <folly/logging/StandardLogHandler.h>
+#include <folly/Conv.h>
+#include <folly/logging/Init.h>
+#include <folly/logging/LogConfigParser.h>
+#include <folly/logging/LogFormatter.h>
+#include <folly/logging/FileHandlerFactory.h>
+#include <folly/logging/StreamHandlerFactory.h>
+#include <folly/logging/LogHandlerFactory.h>
+#include <folly/logging/LogWriter.h>
+#include <folly/logging/LoggerDB.h>
+#include <folly/logging/StandardLogHandler.h>
+#include <folly/logging/StandardLogHandlerFactory.h>
+#include <folly/logging/xlog.h>
+#include <folly/FileUtil.h>
+#include <folly/Random.h>
+#include <folly/ScopeGuard.h>
+#include <folly/hash/SpookyHashV2.h>
+#include <folly/json.h>
+//#include <folly/experimental/TimerFDTimeoutManager.h>
+//#include <folly/experimental/STTimerFDTimeoutManager.h>
+#include <folly/io/async/test/Util.h>
+//#include <folly/experimental/STTimerFDTimeoutManager.h>
+//#include <folly/experimental/TimerFDTimeoutManager.h>
+#include <folly/io/async/test/UndelayedDestruction.h>
+#include <folly/executors/TimedDrivableExecutor.h>
+#include <folly/Conv.h>
+#include <folly/portability/GFlags.h>
+#include <folly/ssl/Init.h>
+
+#include <glog/logging.h>
+
+#if FOLLY_USE_SYMBOLIZER
+#include <folly/experimental/symbolizer/SignalHandler.h> // @manual
+#endif
+#include <folly/portability/GFlags.h>
+
+namespace po = boost::program_options;
+
+static std::shared_ptr<folly::CPUThreadPoolExecutor> CPU_executor;
+
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+{
+    copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
+    return os;
+}
+
+/*
+ * An RAII object to be constructed at the beginning of main() and destructed
+ * implicitly at the end of main().
+ *
+ * The constructor performs the same setup as folly::init(), including
+ * initializing singletons managed by folly::Singleton.
+ *
+ * The destructor destroys all singletons managed by folly::Singleton, yielding
+ * better shutdown behavior when performed at the end of main(). In particular,
+ * this guarantees that all singletons managed by folly::Singleton are destroyed
+ * before all Meyers singletons are destroyed.
+ */
+class Init {
+ public:
+  // Force ctor & dtor out of line for better stack traces even with LTO.
+  FOLLY_NOINLINE Init(int argc, char* argv[], boost::optional<std::string> log_config, bool removeFlags = true);
+  FOLLY_NOINLINE ~Init();
+
+  Init(Init const&) = delete;
+  Init(Init&&) = delete;
+  Init& operator=(Init const&) = delete;
+  Init& operator=(Init&&) = delete;
+};
+
+Init::Init(int argc, char* argv[],
+    boost::optional<std::string> log_config, bool removeFlags) {
+#if FOLLY_USE_SYMBOLIZER
+  // Install the handler now, to trap errors received during startup.
+  // The callbacks, if any, can be installed later
+  folly::symbolizer::installFatalSignalHandler();
+#elif !defined(_WIN32)
+  google::InstallFailureSignalHandler();
+#endif
+
+  // Move from the registration phase to the "you can actually instantiate
+  // things now" phase.
+  folly::SingletonVault::singleton()->registrationComplete();
+
+  // similar to https://github.com/facebook/folly/blob/master/folly/init/Init.cpp#L49
+  // but removed ParseCommandLineFlags
+  // add support fo file logging,
+  // see https://github.com/facebook/folly/blob/master/folly/logging/docs/LogHandlers.md#file-handler-type
+  folly::LoggerDB::get().registerHandlerFactory(
+      std::make_unique<folly::FileHandlerFactory>());
+
+  if(log_config.is_initialized()) {
+    CHECK(!log_config.value().empty())
+      << "invalid (empty) log configuration";
+    folly::initLoggingOrDie(log_config.value());
+  } else {
+    /// \see https://github.com/facebook/folly/tree/master/folly/logging/docs
+    folly::initLoggingOrDie(
+      ".:=INFO:default:x; default=stream:stream=stderr; x=stream:stream=stderr");
+  }
+  auto programName = argc && argv && argc > 0 ? (argv)[0] : "unknown";
+  XLOG(DBG9) << "program name is " << programName;
+  google::InitGoogleLogging(programName);
+
+#if FOLLY_USE_SYMBOLIZER
+  // Don't use glog's DumpStackTraceAndExit; rely on our signal handler.
+  google::InstallFailureFunction(abort);
+
+  // Actually install the callbacks into the handler.
+  folly::symbolizer::installFatalSignalCallbacks();
+#endif
+}
+
+Init::~Init() {
+  folly::SingletonVault::singleton()->destroyInstances();
+}
+
+int main(int argc, char* argv[])
 {
     auto str = folly::format("The answers are {} and {}", 23, 42);
     std::cout << str << std::endl;
+    folly::ProducerConsumerQueue<int> someQueue{100};
+    /// \note no logging before Init!
+    boost::optional<std::string> log_config;
+    Init(argc, argv, log_config);
     return EXIT_SUCCESS;
 }
